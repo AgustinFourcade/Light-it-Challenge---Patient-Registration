ffdc6ceaa5ebf9b352268eef1408ba003b784874
Aguss <agussfourcade@gmail.com>
2025-03-14 12:52:20 -0300

    Create patients method

--- app/Http/Controllers/PatientController.php
+++ app/Http/Controllers/PatientController.php
@@ -4,12 +4,12 @@ namespace App\Http\Controllers;
 
 use App\Models\Patient;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class PatientController extends Controller
 {
-    public function getAllPatients()
-    {
-        $patients = Patient::all(); // Obtiene todos los pacientes de la base de datos
+    public function getAllPatients(): \Illuminate\Http\JsonResponse{
+        $patients = Patient::all();
 
         if ($patients->isEmpty()) {
             return response()->json([
@@ -23,5 +23,30 @@ class PatientController extends Controller
             'message' => 'List of all patients',
             'patients' => $patients
         ], 200);
+    }
+
+    public function createPatient(Request $request): \Illuminate\Http\JsonResponse{
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:50',
+            'email' => 'required|email|unique:patients,email',
+            'address' => 'required|string|min:5|max:25',
+            'phone' => 'required|string|min:8|max:12',
+            'document_photo' => 'required|string|max:15' // <- TODO: Cargar una imagen
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'error' => $validator->errors()
+            ], 400);
+        }
+
+        $patient = Patient::create($validator->valid());
+
+//         // Enviar email de confirmación de manera asíncrona
+
+        return response()->json([
+            'message' => 'Patient registered successfully!',
+            'patient' => $validator->valid()
+        ], 201);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,6 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PatientController;
 
-Route::get('/test', 'App\Http\Controllers\PatientController@getAllPatients');
+Route::get('/index', 'App\Http\Controllers\PatientController@getAllPatients');
+Route::post('/create', 'App\Http\Controllers\PatientController@createPatient');
